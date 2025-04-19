b5c1bc8140eb5e170063f2cad9d6266066be182b
J Park <mrparkonline@gmail.com>
2025-04-19 11:17:11 -0400

    All stats properly displayed and aggregated

--- app.py
+++ app.py
@@ -1,3 +1,67 @@
 import streamlit as st
+import gspread
+import pandas as pd
 
-st.title("Junior Girls Flag Football 2025 Stats")
+# Initialization
+gc = gspread.service_account(filename='./flag_secret.json')
+stat_sheet = gc.open_by_key('1RLdB3ZTcwNRZp0pv-R6piPWJeHmsZA3NJRkZ73nbzxw')
+responses = stat_sheet.get_worksheet(0)
+all_stats = responses.get_all_records()
+stat_df = pd.DataFrame(all_stats).replace('', 0)
+# End of Initialzations
+
+# PASSING DATA
+passing_cols = ['Player', 'Passing_TD', 'Passing_OnePointConversion', 'Passing_TwoPointConversion']
+pass_df = stat_df.filter(items=passing_cols).groupby('Player').sum(numeric_only=True)
+pass_df = pass_df.sort_values(by='Passing_TD', ascending=False)
+pass_df.columns = ['TD', '1 Point Conversions', '2 Point Conversions']
+
+# END OF PASSING DATA
+
+# RECEIVING DATA
+rec_cols = ['Player', 'Receiving_Catches', 'Receiving_TD', 'Receiving_OnePointConversion', 'Receiving_TwoPointConversion']
+rec_df = stat_df.filter(items=rec_cols).groupby('Player').sum(numeric_only=True)
+rec_df = rec_df.sort_values(by='Receiving_Catches', ascending=False)
+rec_df.columns = ['Receptions', 'TD', '1 Point Conversions', '2 Point Conversions']
+
+# END OF RECEIVING DATA
+
+# RUSHING DATA
+run_cols = ['Player', 'Run_TD', 'Run_OnePointConversion', 'Run_TwoPointConversion']
+run_df = stat_df.filter(items=run_cols).groupby('Player').sum(numeric_only=True)
+run_df = run_df.sort_values(by='Run_TD', ascending=False)
+run_df.columns = ['TD', '1 Point Conversions', '2 Point Conversions']
+
+# END OF RUSHING DATA
+
+# DEFENSIVE DATA
+def_cols = ['Player', 'Def_Flag', 'Def_INT', 'Def_TD']
+def_df = stat_df.filter(items=def_cols).groupby('Player').sum(numeric_only=True)
+def_df = def_df.sort_values(by='Def_Flag', ascending=False)
+def_df.columns = ['Flags', 'Interceptions', 'TD']
+
+# END OF DEFENSIVE DATA
+
+# Kick Return
+kr_cols = ['Player', 'KR_TD']
+kr_df = stat_df.filter(items=kr_cols).groupby('Player').sum(numeric_only=True)
+kr_df = kr_df.sort_values(by='KR_TD', ascending=False)
+kr_df.columns = ['TD']
+
+# Streamlit App
+st.title("GWW Jr Girls Flag Football 2025")
+
+st.write("Passing Stats")
+st.write(pass_df)
+
+st.write("Rushing Stats")
+st.write(run_df)
+
+st.write("Receiving Stats")
+st.write(rec_df)
+
+st.write("Defensive Stats")
+st.write(def_df)
+
+st.write("Kick Return")
+st.write(kr_df)
